35a5488d06485a3120669d6d4a8a82fe86f122cb
Andreea Hodrea <andreeahodrea23@gmail.com>
2022-11-05 15:34:58 +0200

    Add project files.

---
+++ Migrations/20221104125936_publisher.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace lab2.Migrations
+{
+    public partial class publisher : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
